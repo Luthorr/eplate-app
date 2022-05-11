123140ed2651a1e068ede9dc4d3261d2158fbd75
Przemysław Stupak <therixmeisterr@gmail.com>
2022-05-11 15:43:50 +0200

    Added react-query integration and scroll to top after url param changes

--- app/src/providers/AppProviders.tsx
+++ app/src/providers/AppProviders.tsx
@@ -1,8 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import ScrollToTop from 'hooks/useScrollToTop';
 import { AppProvidersProps } from './AppProviders.types';
 
+const queryClient = new QueryClient();
+
 const AppProviders = ({ children }: AppProvidersProps) => (
-  <Router>{children}</Router>
+  <QueryClientProvider client={queryClient}>
+    <Router>
+      <ScrollToTop />
+      {children}
+    </Router>
+  </QueryClientProvider>
 );
 
 export default AppProviders;
